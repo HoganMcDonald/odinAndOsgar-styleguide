9236b595212553b58e345ff61882bf6f0670e653
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-21 14:16:59 -0600

    adjusted measure of scrolling element percentage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,22 @@
-function calcRange(min, max, percent) {
-  return (percent * 0.01) * (max - min) + min;
+function calcRange(end, start, percent) {
+  return (percent * 0.01) * (start - end) + end;
 }
 
 $(document).ready(()=> {
   // elements for scroll effects
   const logo = $('.logo');
-  const windowH = $('body').height()/2;
+  const sidePanel = $('.sidePanel');
+  console.log(document.scrollingElement);
+
+  //height of the viewport
+  const windowH = document.scrollingElement.scrollTopMax;
 
   // scroll effects
   $(window).on('scroll', ()=> {
     const bScroll = document.scrollingElement.scrollTop;
 
     let scrollPercent = ((windowH - bScroll) / windowH) * 100;
+    console.log(scrollPercent);
 
     const logoH = calcRange(15, 50, scrollPercent);
     const logoX = calcRange(25, 50, scrollPercent);
@@ -19,6 +24,9 @@ $(document).ready(()=> {
     logo.css('height', `${logoH}vh`)
     logo.css('top', `${logoY}vh`)
     logo.css('left', `${logoX}vw`)
+
+    const sidePanelW = calcRange(50, 100, scrollPercent);
+    sidePanel.css('width', `${sidePanelW}vw`)
   });
 
 });
