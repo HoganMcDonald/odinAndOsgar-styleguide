0fa6bbaad139159e85a26cecdd46257da220e9d0
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-22 23:28:58 -0600

    setup for partial html

--- src/scripts/modules/jquery-effects.js
+++ src/scripts/modules/jquery-effects.js
@@ -2,45 +2,47 @@ function calcRange(end, start, percent) {
   return (percent * 0.01) * (start - end) + end;
 }
 
-$(document).ready(function() {
-  // elements for scroll effects
-  const logo = $('.logo');
-  const sidePanel = $('.sidePanel');
-  const sectionContainer = $('.section-container');
+//height of the viewport
+const windowH = document.scrollingElement.scrollTopMax;
 
-  //height of the viewport
-  const windowH = document.scrollingElement.scrollTopMax;
+// elements for scroll effects
+const logo = $('.logo');
+const sidePanel = $('.sidePanel');
+const sectionContainer = $('.section-container');
 
-  // gradient changes
-  $('.section').click( function() {
-    const top = $(this).index() * 100
-    $('.sidePanel').css('top', `-${top}vh`);
-  });
+function scrollSet() {
+  const bScroll = document.scrollingElement.scrollTop;
 
-  // scroll effects
-  $(window).on('scroll', ()=> {
-    const bScroll = document.scrollingElement.scrollTop;
+  let scrollPercent = ((windowH - bScroll) / windowH) * 100;
 
-    let scrollPercent = ((windowH - bScroll) / windowH) * 100;
+  // logo
+  const logoH = calcRange(15, 50, scrollPercent);
+  const logoX = calcRange(15, 50, scrollPercent);
+  const logoY = calcRange(10, 50, scrollPercent);
+  logo.css('height', `${logoH}vh`)
+  logo.css('top', `${logoY}vh`)
+  logo.css('left', `${logoX}vw`)
 
-    // logo
-    const logoH = calcRange(15, 50, scrollPercent);
-    const logoX = calcRange(15, 50, scrollPercent);
-    const logoY = calcRange(10, 50, scrollPercent);
-    logo.css('height', `${logoH}vh`)
-    logo.css('top', `${logoY}vh`)
-    logo.css('left', `${logoX}vw`)
+  // side-panel
+  const sidePanelW = calcRange(30, 100, scrollPercent);
+  sidePanel.css('width', `${sidePanelW}vw`)
 
-    // side-panel
-    const sidePanelW = calcRange(30, 100, scrollPercent);
-    sidePanel.css('width', `${sidePanelW}vw`)
+  // section-container
+  const sectionContainerT = calcRange(60, 150, scrollPercent);
+  const sectionContainerO = calcRange(1, -1, scrollPercent).toFixed(2);
+  sectionContainer.css('top', `${sectionContainerT}vh`)
+  sectionContainer.css('opacity', `${sectionContainerO}`)
+}
 
-    // section-container
-    const sectionContainerT = calcRange(60, 150, scrollPercent);
-    const sectionContainerO = calcRange(1, -1, scrollPercent).toFixed(2);
-    sectionContainer.css('top', `${sectionContainerT}vh`)
-    sectionContainer.css('opacity', `${sectionContainerO}`)
+scrollSet();
 
-  });
+// scroll effects
+$(window).on('scroll', ()=> {
+  scrollSet();
+});
 
+// gradient changes
+$('.section').click( function() {
+  const top = $(this).index() * 100
+  $('.sidePanel').css('top', `-${top}vh`);
 });
